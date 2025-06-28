1309bb62adc8bfd73bfaab6072527510f6b6ab3d
Neel-020 <maheshgami12977@gmail.com>
2025-06-28 11:06:57 +0530

    Third commit

--- frontend/src/utils/constant.js
+++ frontend/src/utils/constant.js
@@ -1,4 +1,5 @@
-export const USER_API_END_POINT = import.meta.env.VITE_REACT_APP_BACKEND_BASEURL + "/api/v1/user";
-export const JOB_API_END_POINT = import.meta.env.VITE_REACT_APP_BACKEND_BASEURL + "/api/v1/job";
-export const APPLICATION_API_END_POINT = import.meta.env.VITE_REACT_APP_BACKEND_BASEURL + "/api/v1/application";
-export const COMPANY_API_END_POINT = import.meta.env.VITE_REACT_APP_BACKEND_BASEURL + "/api/v1/company";
+export const BASE_BACKEND_URL = "https://talent-nest-backend.onrender.com";
+export const USER_API_END_POINT = `${BASE_BACKEND_URL}/api/v1/user`;
+export const JOB_API_END_POINT = `${BASE_BACKEND_URL}/api/v1/job`;
+export const APPLICATION_API_END_POINT = `${BASE_BACKEND_URL}/api/v1/application`;
+export const COMPANY_API_END_POINT = `${BASE_BACKEND_URL}/api/v1/company`;
